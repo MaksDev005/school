813880740acf71874e82c8e3c5ab3152a711e15d
MaksDev005 <maksatmatkaziev759@gmail.com>
2022-10-01 20:43:54 +0600

    init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector(".about-btn button")
+btn.addEventListener("click", () => {
+    document.querySelector(".about-modal").style.display = "block"
+})
+let close = document.querySelector(".about-modal-bg-square div")
+close.addEventListener("click", () => {
+    document.querySelector(".about-modal").style.display = "none"
+})
+
